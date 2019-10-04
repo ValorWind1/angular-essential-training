d8e0c1cd9cd60f560dc30b6491bffb3bb96c06c2
ValorWind1 <skyjdani@gmail.com>
2019-10-04 11:49:52 -0400

    master branch update chapter 3

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+// bootstrap logic on this file 
+import { platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
